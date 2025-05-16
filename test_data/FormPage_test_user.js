b548d9fece864302667da99586ad984a3886f6ef
“FirstName <nick.sinazhenski@student.ehu.lt>
2025-05-16 19:51:04 +0200

    fix: resolve bug with extra space

---
+++ test_data/FormPage_test_user.js
@@ -0,0 +1,15 @@
+import { faker } from '@faker-js/faker';
+
+const FormPage_test_user = {
+  firstName:faker.person.firstName(),
+  lastName: faker.person.lastName(),
+  email: faker.internet.email(),
+  gender: faker.helpers.arrayElement([1, 2, 3]),
+  mobile: faker.phone.number({style: 'international'}).slice(-10),
+}
+export default FormPage_test_user;
+
+
+
+
+console.log(FormPage_test_user.mobile);
---
+++ test_data/TextBoxPage_test_user.js
@@ -0,0 +1,11 @@
+import { faker } from '@faker-js/faker';
+
+const TextBox_test_user = {
+  firstName: faker.person.firstName(),
+  lastName: faker.person.lastName(),
+  email: faker.internet.email(),
+  gender: faker.helpers.arrayElement([1, 2, 3]),
+  currentAddress: faker.location.streetAddress(),
+  permanentAddress: faker.location.streetAddress(),
+}
+export default TextBox_test_user;
